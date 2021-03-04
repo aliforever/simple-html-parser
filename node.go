a746970b8d05a33db6408ebcdffa257b5cdd2360
aliforever <alipadida@live.com>
2021-03-05 00:17:36 +0330

    added extract tags

--- node.go
+++ node.go
@@ -2,11 +2,13 @@ package simple_html_parser
 
 import (
 	"errors"
+	"io"
 	"regexp"
 	"strings"
 )
 
 type node struct {
+	rawHtml        string
 	html           string
 	tag            string
 	start          string
@@ -23,6 +25,7 @@ func newNode(html string, beginTag string) (n *node, err error) {
 	}
 	tagR := regexp.MustCompile(`<([^\s]+).*?>`).FindStringSubmatch(beginTag)
 	n = &node{
+		rawHtml:        html,
 		start:          html[i : i+len(beginTag)],
 		html:           html[i+len(beginTag):],
 		tag:            strings.TrimSpace(tagR[1]),
@@ -32,71 +35,91 @@ func newNode(html string, beginTag string) (n *node, err error) {
 	return
 }
 
-func (n *node) traverse() (result string, err error) {
+func (n *node) readByte(r *strings.Reader) (b byte, eof bool, err error) {
+	b, err = r.ReadByte()
+	if err != nil && err == io.EOF {
+		eof = true
+		err = nil
+	}
+	return
+}
+
+func (n *node) readTagByte(r *strings.Reader) (data string, isOpeningTag, isClosingTag bool, eof bool, err error) {
+	var (
+		b          byte
+		tagBytes   = []byte(n.tag)
+		slashByte  = byte('/')
+		endTagByte = byte('>')
+	)
+	for _, tagByte := range tagBytes {
+		if eof {
+			return
+		}
+		b, eof, err = n.readByte(r)
+		if err != nil {
+			return
+		}
+		data += string(b)
+		if !eof && b == slashByte {
+			isClosingTag = true
+			b, eof, err = n.readByte(r)
+			if err != nil {
+				return
+			}
+			data += string(b)
+		}
+		if !eof && b != tagByte {
+			isClosingTag = false
+			isOpeningTag = false
+			return
+		} else {
+			if !isClosingTag {
+				isOpeningTag = true
+			}
+		}
+	}
+
+	b = 0
+	for b != endTagByte && !eof {
+		b, eof, err = n.readByte(r)
+		if err != nil {
+			return
+		}
+		data += string(b)
+	}
+	if b != endTagByte {
+		err = errors.New("eng tag not found")
+	}
+	return
+}
+
+func (n *node) traverse() (endTagIndex int, err error) {
 	r := strings.NewReader(n.html)
-	var tagBytes = []byte(n.tag)
 
 	var startTagByte = byte('<')
-	var endTagByte = byte('>')
-	var endTagSeparatorByte = byte('/')
 	var char byte
+	var isOpening, isClosing bool
+	var eof bool
 
-Loop:
 	for char, err = r.ReadByte(); err == nil; char, err = r.ReadByte() {
-		result += string(char)
-		isEnding := false
-
 		if char == startTagByte {
-			for _, tagByte := range tagBytes {
-				char, err = r.ReadByte()
-				if err != nil {
-					result = ""
-					err = errors.New("invalid html tag " + err.Error())
-					return
-				}
-				result += string(char)
-				if char == endTagSeparatorByte {
-					isEnding = true
-					char, err = r.ReadByte()
-					if err != nil {
-						result = ""
-						err = errors.New("invalid html tag " + err.Error())
-						return
-					}
-					result += string(char)
-				}
-				if char != tagByte {
-					continue Loop
-				}
+			_, isOpening, isClosing, eof, err = n.readTagByte(r)
+			if err != nil || eof {
+				return
 			}
-
-			for true {
-				char, err = r.ReadByte()
-				if err != nil {
-					err = errors.New("invalid html tag " + err.Error())
-					result = ""
-					return
-				}
-				result += string(char)
-				if char == endTagByte {
-					break
-				}
-			}
-
-			if !isEnding {
+			if isOpening {
 				n.sameTagCounter++
-			} else {
+			} else if isClosing {
 				n.sameTagCounter--
 			}
 			n.pause = true
 
 			if n.sameTagCounter == 0 && n.pause {
-				result = n.start + result
+				endTagIndex = int(r.Size()) - r.Len()
 				return
 			}
 		}
 	}
-	result = ""
 	err = errors.New("invalid html")
 	return
 }
--- parser.go
+++ parser.go
@@ -14,5 +14,32 @@ func (p *Parser) ExtractTag(tagBegin string) (tag string, err error) {
 	if err != nil {
 		return
 	}
-	return n.traverse()
+	var endTagIndex int
+	endTagIndex, err = n.traverse()
+	if err != nil {
+		return
+	}
+	tag = n.start + n.html[:endTagIndex]
+	return
+}
+
+func (p *Parser) ExtractTags(tagBegin string) (tags []string, err error) {
+	for true {
+		var n *node
+		n, err = newNode(p.HTML, tagBegin)
+		if err != nil {
+			if len(tags) > 0 {
+				err = nil
+			}
+			return
+		}
+		var endTagIndex int
+		endTagIndex, err = n.traverse()
+		if err != nil {
+			return
+		}
+		tags = append(tags, n.start+n.html[:endTagIndex])
+		p.HTML = n.html[endTagIndex:]
+	}
+	return
 }
